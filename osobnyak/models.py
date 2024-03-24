79fe67eb5adc8f2db017963398b568f9f303966f
Denis Buryi <denis@buryi.ru>
2024-03-24 21:16:49 +0700

    i18n

--- osobnyak/models.py
+++ osobnyak/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class OrderingMixin(models.Model):
-    ordering = models.SmallIntegerField(default=10)
+    ordering = models.SmallIntegerField(default=10, verbose_name='порядок')
 
     class Meta:
         abstract = True
@@ -17,8 +17,8 @@ class Category(OrderingMixin):
         HOUSES = 0, _('Дома, коттеджи')
         MODULES = 1, _('Модульные строения')
 
-    page = models.SmallIntegerField(choices=Page.choices)
-    title = models.CharField(max_length=127)
+    page = models.SmallIntegerField(choices=Page.choices, verbose_name='страница')
+    title = models.CharField(max_length=127, verbose_name='название')
     slug = models.SlugField()
 
     class Meta:
@@ -26,17 +26,19 @@ class Category(OrderingMixin):
             'page',
             'ordering',
         ]
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
 
     def __str__(self):
         return f'{self.get_page_display()} - {self.title}'
 
 
 class Product(OrderingMixin):
-    title = models.CharField(max_length=127)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price = models.CharField(max_length=31)
-    description = models.TextField()
-    enabled = models.BooleanField(default=True)
+    title = models.CharField(max_length=127, verbose_name='название')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.CharField(max_length=31, verbose_name='цена')
+    description = models.TextField(verbose_name='описание')
+    enabled = models.BooleanField(default=True, verbose_name='включено')
 
     def __str__(self):
         return self.title
@@ -47,8 +49,14 @@ class Product(OrderingMixin):
             'category__ordering',
             'ordering',
         ]
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
 
 
 class ProductImage(OrderingMixin):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='products')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images', verbose_name='продукт')
+    image = models.ImageField(upload_to='products', verbose_name='картинка')
+
+    class Meta:
+        verbose_name = "картинка продукта"
+        verbose_name_plural = "картинки продуктов"
